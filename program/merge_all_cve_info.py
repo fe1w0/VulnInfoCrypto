4fa56b8d0ee57036ded27c0b0f682b3564b7d4f1
fe1w0 <xzasliuxinrong@gmail.com>
2023-05-06 16:00:26 +0800

    merge all cve info

--- program/merge_all_cve_info.py
+++ program/merge_all_cve_info.py
@@ -1,5 +1,7 @@
 import yaml
-
+import re
+from collections import defaultdict
+import matplotlib.pyplot as plt
 
 def read_yaml_info(file_name):
     with open(file_name, 'r') as f:
@@ -7,7 +9,27 @@ def read_yaml_info(file_name):
     return file_info
 
 
+def get_year_from_cve_id(cve_id):
+    year_match = re.search(r'CVE-(\d{4})-', cve_id)
+    if year_match:
+        year = int(year_match.group(1))
+    else:
+        # Handle the case where no year is found
+        year = None
+    return year
+
+
+def get_picture(cve_counts):
+    plt.bar(cve_counts.keys(), cve_counts.values())
+    plt.xlabel('Year')
+    plt.ylabel('CVE Count')
+    plt.title('Number of CVEs per Year')
+    plt.show()
+        
+
 def main():
+    cve_counts = defaultdict(int)
+    
     file_name = "output/result.yml"
     yml_result = read_yaml_info(file_name)
     
@@ -20,11 +42,15 @@ def main():
     file_name = "output/cwe_result.yml"
     yml_cwe_result = read_yaml_info(file_name)
     
-    for cwe in yml_cwe_result:
-        for cve in yml_cwe_result[cwe]:
-            result_cves.append(yml_cwe_result[cwe][cve]['cve_id'])
-    
-    print(len(result_cves))
+    # for cwe in yml_cwe_result:
+    #     for cve in yml_cwe_result[cwe]:
+    #         result_cves.append(yml_cwe_result[cwe][cve]['cve_id'])
+        
+    for cve_id in result_cves:
+        cve_year = get_year_from_cve_id(cve_id)
+        cve_counts[cve_year] += 1
+        
+    get_picture(cve_counts)
         
 if __name__ == "__main__":
     main()
